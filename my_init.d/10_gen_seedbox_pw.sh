eae3b4bc95b544fcc5b102abbcabd2c224c2f756
Xadozuk <xadozuk@gmail.com>
2020-04-29 12:57:33 +0200

    Update password generation to accept ENV var

--- my_init.d/10_gen_seedbox_pw.sh
+++ my_init.d/10_gen_seedbox_pw.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-PASSWORD_FILE=/etc/container_environment/PASSSWORD_GENERATED
-
-if [ ! -f $PASSWORD_FILE ]; then
+if [ -n "$SFTP_PASSWORD" ]; then
+    PASSWORD=$SFTP_PASSWORD
+else
+    printf "env var SFTP_PASSWORD not set, generating a new password\n"
     PASSWORD=$(</dev/urandom tr -dc 'A-Za-z0-9@#$%&_+=' 2>/dev/null | head -c16)
-    usermod --password $(echo $PASSWORD | openssl passwd -1 -stdin) seedbox
-
     printf "Password: %s\n" $PASSWORD
-
-    touch $PASSWORD_FILE
 fi
+
+usermod --password $(echo $PASSWORD | openssl passwd -1 -stdin) seedbox
